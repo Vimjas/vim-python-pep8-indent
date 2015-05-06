668edf8d06e07cb74d8dd9680c65f290b61c10f0
Daniel Hahler <git@thequod.de>
2015-05-06 16:30:12 +0200

    Vimrunner: doc for config.reuse_server=false
    I've tried to set it to true by default, but it is still buggy.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,13 @@ require 'vimrunner'
 require 'vimrunner/rspec'
 
 Vimrunner::RSpec.configure do |config|
+  # Use a single Vim instance for the test suite. Set to false to use an
+  # instance per test (slower, but can be easier to manage).
   # FIXME: reuse_server = true seems to hang after a certain number of test cases
-  config.reuse_server = false
+  #  - Travis CI hangs after 15 successful tests.
+  #  - Locally it may hang also, with Vim and Xorg using 100% CPU.
+  # Therefore default to false in both cases.
+  config.reuse_server = ENV['CI'] ? false : false
 
   config.start_vim do
     vim = Vimrunner.start
